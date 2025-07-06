e57437c056e1cabaee7d14aa61dbb2ca9a675143
Diego <cbdiegox@gmail.com>
2025-07-06 16:14:26 -0600

    test

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,7 @@ async function bootstrap() {
     const message = 'Funciona OK';
     logger.log(message);
     res.send(message);
+    console.log('deploy tessprueba nuevat');
   });
 
   app.useGlobalFilters(new HttpExceptionFilter());
